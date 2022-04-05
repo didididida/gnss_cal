e1eb64818c92e26b2d01a433d81e1b4f47d50e11
hai <di.haidi@outlook.com>
2022-04-05 17:38:34 +0200

    multipath part

--- src/gnss_cal_node.cpp
+++ src/gnss_cal_node.cpp
@@ -95,18 +95,64 @@ void rangemeas_cb(const gnss_comm::GnssMeasMsgConstPtr &meas_msg){
         uint32_t sys = gnss_comm::satsys(obs->sat,NULL);
 
         //only process gps/bds/gal/glo
-        if(sys!=SYS_GPS&&sys!=SYS_BDS&&sys!=SYS_GAL&&sys!=SYS_GLO)
+        if(sys!=SYS_GPS&&sys!=SYS_BDS&&sys!=SYS_GAL) //&&sys!=SYS_GLO)
         continue;
-
+        
         //To count the number of satellites
         if(gnss_comm::satsys(obs->sat,NULL)==0)
         continue;
         if(obs->freqs.empty())continue;
+       
+        
+        /*calculte multipath erorr (Attention not NLOS here)
+          according to a paper upload in the github 
+          make true reciever can recieve more than one band   
+        */
+        double freq_1 = 0.0;
+        double freq_2 = 0.0;
 
-        //only use L1
+        if(sys==SYS_GPS){
+            freq_1 = FREQ1;
+            freq_2 = FREQ2;
+        }else if(sys==SYS_BDS){
+            freq_1 = FREQ1_BDS;
+            freq_2 = FREQ2_BDS;
+        }else if(sys== SYS_GLO){
+            freq_1 = FREQ1_GLO;
+            freq_2 = FREQ2_GLO;
+        }else if(sys ==SYS_GAL){
+            freq_1 = FREQ1;
+            freq_2 = FREQ5;
+        }
+        
         int freq_idx = -1;
         gnss_comm::L1_freq(obs,&freq_idx);
         if(freq_idx<0)continue;
+
+        // find the index for L2/BD2/E5
+        int freq2_idx = -1;
+        for(int i = 0;i<obs->freqs.size();i++){
+             if (obs->freqs[i] >= freq_2 && obs->freqs[i] <= freq_2)
+            {
+                if (freq2_idx != NULL)
+                    freq2_idx = i;
+            }
+        }
+        if(freq2_idx<0)
+        {
+            ROS_INFO("No frequence L2");
+            continue;
+        }
+        
+        //carrier phase for diffrent frequency
+        double CP_1 = obs->cp[freq_idx];
+        double CP_2 = obs->cp[freq2_idx];
+        
+        double a = pow(CP_1,2)/(pow(CP_1,2)-pow(CP_2,2));
+        //Miltipath estimated Mp;
+        //Mp here includes multipath error and noise
+        double Mp = obs->psr[freq_idx]- CP_1 + 2*a*(CP_1-CP_2);
+
         double obs_time = gnss_comm::time2sec(obs->time);
 
         if(!sat2time_index.count(obs->sat)){
@@ -153,11 +199,12 @@ void rangemeas_cb(const gnss_comm::GnssMeasMsgConstPtr &meas_msg){
         
         //calculate the ionosphere delay and troposphere delay
         double iono_delay = 0;
-        double trop_delay = 0;
+       
        
         double azel[2] = {0, M_PI/2.0};
         Eigen::Vector3d rcv_lla = gnss_comm::ecef2geo(pos);
         gnss_comm::sat_azel(pos,sat_ecef,azel);
+        double trop_delay = 0.0;
         trop_delay = gnss_comm::calculate_trop_delay(obs->time,rcv_lla,azel);
         
         {
@@ -166,7 +213,9 @@ void rangemeas_cb(const gnss_comm::GnssMeasMsgConstPtr &meas_msg){
         }
         //calculated pseudorange, eliminate the error of iono, trop, group delay 
         //and satellite clock drift.
-        double psr = obs->psr[freq_idx]-trop_delay-iono_delay+svdt*LIGHT_SPEED-tgd*LIGHT_SPEED;    
+        double psr = obs->psr[freq_idx]- trop_delay - iono_delay+svdt*LIGHT_SPEED-tgd*LIGHT_SPEED;    
+        //eliminate the estimated mulipath error
+        psr -= Mp;
         gnss_cal::gnssToU submsg;
         submsg.CN0 = obs->CN0;
         submsg.psr = psr;
